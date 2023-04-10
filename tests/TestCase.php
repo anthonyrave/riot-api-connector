e6b6cbd86c66cd389cc83fc5994718891914e83a
Anthony Rave <anthony.rave@icloud.com>
2023-04-11 01:42:25 +0200

    wip: Tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,11 +3,13 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\Concerns\InteractsWithConsole;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use InteractsWithConsole;
+    use RefreshDatabase;
 
     protected function getPackageProviders($app): array
     {
