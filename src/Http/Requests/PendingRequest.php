170f8d052fe323be26eaddeff1d2370525d009ba
Anthony Rave <anthony.rave@icloud.com>
2023-04-24 14:55:58 +0200

    feat: Adding InvalideApiKeyException

--- src/Http/Requests/PendingRequest.php
+++ src/Http/Requests/PendingRequest.php
@@ -2,7 +2,9 @@
 
 namespace RiotApiConnector\Http\Requests;
 
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\Http;
+use RiotApiConnector\Exceptions\InvalidApiKeyException;
 use RiotApiConnector\Models\Region;
 
 // TODO Handle Request exceptions and add hints (API Token...)
@@ -14,10 +16,10 @@ class PendingRequest
 
     public ?Region $region;
 
-    public function __construct(
-    ) {
-    }
-
+    /**
+     * @throws InvalidApiKeyException
+     * @throws RequestException
+     */
     public function fetch()
     {
         $response = Http::withHeaders(
@@ -27,11 +29,18 @@ class PendingRequest
             ->withUrlParameters($this->url_params)
             ->get($this->endpoint);
 
-        return $response->json();
+        try {
+            return $response->throw()->json();
+        } catch (RequestException $e) {
+            if ($response->status() === 403) {
+                throw new InvalidApiKeyException('Your API Key is invalid. Please check if it has expired.', 403);
+            }
+            throw $e;
+        }
     }
 
     private function getBaseUrl(): string
     {
-        return ($this->region ? 'https://'.$this->region->name.'.' : 'https://').config('riot.url');
+        return ($this->region ? 'https://' . $this->region->name . '.' : 'https://') . config('riot.url');
     }
 }
